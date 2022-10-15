191ef8fd1e5da8a9aa6ffc9aa4536aef370cc385
Olawale Aro <aro.olawale1@gmail.com>
2022-10-15 22:27:38 +0100

    move use(body-parser.json()) to index from routes

--- routes/punter.routes.js
+++ routes/punter.routes.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser')
 const router = express.Router()
 const { addGame, newBet } = require('../controllers/punter.controllers')
 
-router.use(bodyParser.json())
-
 router.post('/addgame', addGame)
 
 router.post('/newbet', newBet)
